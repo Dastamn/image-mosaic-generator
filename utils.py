dc026eb039818b6ed342baa172e51171282a5d98
Dastamn <mezhoudi.nazim@gmail.com>
2022-04-23 22:40:17 +0200

    Fixed stuff
    - Fixed limit in path random sampling
    - Updated image resizing and cropping
    - Added source set loading

--- utils.py
+++ utils.py
@@ -1,11 +1,15 @@
 import random
+import re
 from glob import glob
-from typing import List, Tuple
+from typing import List, Tuple, Union
 
 import numpy as np
 from numpy.lib.stride_tricks import as_strided
 from PIL import Image
+from tqdm import tqdm
 from typing_extensions import Literal
+
+from augment import to_bgr, to_grayscale, to_high_contrast, to_negative
 
 
 def get_image_paths(dir: str, ext: List[str] = ['jpg', 'jpeg', 'png'], limit: int = None) -> List[str]:
@@ -13,11 +17,11 @@ def get_image_paths(dir: str, ext: List[str] = ['jpg', 'jpeg', 'png'], limit: in
     ext = tuple(f'.{i}' for item in ext for i in item)
     paths = glob(f'{dir}/**/*', recursive=True)
     if limit:
-        paths = random.sample(paths, limit)
+        paths = random.sample(paths, min(len(paths), limit))
     return list(filter(lambda x: x.endswith(ext), paths))
 
 
-def image_as_array(path: str, size: int = None) -> np.ndarray:
+def image_as_array(path: str, size: int = None, crop: bool = False) -> np.ndarray:
     im = Image.open(path)
     w, h = im.size
     w_start, h_start = 0, 0
@@ -25,13 +29,29 @@ def image_as_array(path: str, size: int = None) -> np.ndarray:
     if size:
         # resize so that smallest dim matches 'size'
         w, h = (size*w//h, size) if w > h else (size, size*h//w)
-        w_start, h_start = w//2 - min(size, w)//2, h//2 - min(size, h)//2
-        w_size, h_size = size, size
-        im = im.resize((w, h))
+        im = im.resize((w, h), Image.ANTIALIAS)
+        if crop:
+            w_start, h_start = w//2 - min(size, w)//2, h//2 - min(size, h)//2
+            w_size, h_size = size, size
     im = np.asarray(im, dtype=np.uint8)
     # center crop
     im = im[h_start:h_start+h_size, w_start:w_start+w_size, :]
     return im
+
+
+def load_source(src_path: str, augment: bool = False, size: int = None, limit: int = None, ext: Union[str, List[str]] = ['jpg', 'jpeg', 'png']) -> Tuple[np.ndarray, np.ndarray]:
+    if isinstance(ext, str):
+        ext = re.split(r', *', ext)
+    paths = get_image_paths(src_path, ext, limit)
+    images = np.asarray([image_as_array(path, size, crop=True)
+                         for path in tqdm(paths)])
+    cat = images if not augment \
+        else np.concatenate((images,
+                             to_grayscale(images),
+                             to_high_contrast(images),
+                             to_bgr(images),
+                             to_negative(images)), axis=0)
+    return cat, np.mean(cat, axis=(1, 2))
 
 
 def tile(x: np.ndarray, window: Tuple[int, int] = (2, 2), stride: int = 2) -> np.ndarray:
